606d147d0db066f0ba22820c2e1cba21197190ea
2403586777@qq.com <2403586777@qq.com>
2022-01-30 21:06:56 +0800

    woc

---
+++ src/main/java/com/woc/service/BookServiceImpl.java
@@ -0,0 +1,44 @@
+package com.woc.service;
+
+
+import com.woc.dao.BookMapper;
+import com.woc.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+    //service调dao层，组合Dao
+    //@Autowired
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBooks() {
+        return bookMapper.queryAllBooks();
+    }
+
+    public List<Books> queryBook(String bookName) {
+        return bookMapper.queryBook(bookName);
+    }
+    public Books queryBookByName(String bookName) {
+        return bookMapper.queryBookByName(bookName);
+    }
+}
